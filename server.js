2e7cff64adffdb8ca43337c94357561f465909e8
abarrios003 <alex.barrios.ureta@gmail.com>
2017-03-20 17:35:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
 });
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '172.30.163.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
